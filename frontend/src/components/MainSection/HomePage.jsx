4bafd97d529518092e4a77d0bcaa883e6ec81f41
ganji <ganjipraveen444@gmail.com>
2026-02-20 19:42:55 +0530

    status boxes are updated

--- frontend/src/components/MainSection/HomePage.jsx
+++ frontend/src/components/MainSection/HomePage.jsx
@@ -81,7 +81,7 @@ const HomePage = ({adminTasksStats,setRefreshData }) => {
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
           {statusBoxes.map(([title, value], index) => (
             <div
-              className="border border-black w-[120px] h-[70px] text-center sm:w-[200px] sm:h-[100px] sm:text-lg"
+              className="border border-black w-[120px] text-center sm:w-[200px]  sm:text-lg"
               key={index}
             >
               <p className="p-1 font-bold text-green-600">{title}</p>
